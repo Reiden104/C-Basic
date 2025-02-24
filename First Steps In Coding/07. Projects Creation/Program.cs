b62d3085d0ab73de06522fb555f6ed0ae1f2e8c7
Reiden104 <v.v.andreev06@gmail.com>
2025-02-24 15:12:16 +0200

    Add files via upload

---
+++ First Steps In Coding/07. Projects Creation/Program.cs
@@ -0,0 +1,5 @@
+﻿string name = Console.ReadLine();
+int project = int.Parse(Console.ReadLine());
+int timeSpended = project * 3;
+Console.WriteLine($"The architect {name} will need {timeSpended} " +
+    $"hours to complete {project} project/s.");
