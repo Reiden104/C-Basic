739eab927aca2ba2ed2b38c8e2d1d6dc57784db7
Reiden104 <v.v.andreev06@gmail.com>
2025-02-24 15:13:05 +0200

    Add files via upload

---
+++ First Steps In Coding/08. Pet Shop/Program.cs
@@ -0,0 +1,6 @@
+﻿int dogFood = int.Parse(Console.ReadLine());
+int catFood = int.Parse(Console.ReadLine());
+double dogPriceFood = dogFood * 2.50;
+int catPriceFood = catFood * 4;
+double allPrice = dogPriceFood + catPriceFood;
+Console.WriteLine(allPrice + " lv.");
---
+++ First Steps In Coding/09. Yard Greening/Program.cs
@@ -0,0 +1,6 @@
+﻿double grass = double.Parse(Console.ReadLine());
+double price = grass * 7.61;
+double discount = 0.18 * price;
+double priceDiscount = price - discount;
+Console.WriteLine($"The final price is: {priceDiscount} lv.");
+Console.WriteLine($"The discount is: {discount} lv.");
